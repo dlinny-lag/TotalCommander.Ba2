33daeede89e98eb1b2fec4b53fd934e9c691a440
Dlinny_Lag <dlinny.lag@gmail.com>
2025-10-26 17:51:10 +0500

    migrated to .NET9 to enable x86 support. added x86 build

---
+++ P1X.TotalCommander.Ba2/publish-x86.ps1
@@ -0,0 +1,7 @@
+﻿cd $PSScriptRoot
+dotnet publish -r win-x86 -p:NativeLib=Shared
+mkdir $PSScriptRoot\bin\Release\net9.0\win-x86\publish\TotalCommander.Ba2 -Force
+copy $PSScriptRoot\bin\Release\net9.0\win-x86\publish\TotalCommander.Ba2.dll $PSScriptRoot\bin\Release\net9.0\win-x86\publish\TotalCommander.Ba2\TotalCommander.Ba2.wcx
+copy $PSScriptRoot\pluginst.inf $PSScriptRoot\bin\Release\net9.0\win-x86\publish\TotalCommander.Ba2\pluginst.inf
+Compress-Archive -Path $PSScriptRoot\bin\Release\net9.0\win-x86\publish\TotalCommander.Ba2\* -DestinationPath $PSScriptRoot\bin\Release\net9.0\win-x86\publish\TotalCommander.Ba2.zip -CompressionLevel Optimal -Force
+echo done!
